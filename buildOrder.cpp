75eaedec08e62e710392f8f303669ad481afde11
Manarsh Nowbut <91077872+siiinx10@users.noreply.github.com>
2022-03-30 23:06:36 -0230

    Update buildOrder.cpp

--- buildOrder.cpp
+++ buildOrder.cpp
@@ -14,3 +14,96 @@
             "Zerg_3HatchMuta"       : { "Race" : "Zerg",    "OpeningBuildOrder" : ["Drone", "Drone", "Drone", "Drone", "Overlord", "Drone", "Drone", "Drone", "Drone", "Hatchery", "Drone", "Drone", "Spawning_Pool", "Drone", "Drone", "Extractor", "Drone", "Drone", "Drone", "Drone", "Drone", "Drone","Lair", "Drone", "Drone", "Drone", "Drone", "Drone", "Drone", "Drone", "Drone", "Drone", "Drone", "Spire", "Overlord", "Drone", "Overlord", "Hatchery", "Drone", "Drone", "Drone", "Drone", "Drone", "Drone", "Drone", "Drone", "Drone", "Drone", "Mutalisk", "Mutalisk", "Mutalisk", "Mutalisk", "Mutalisk", "Mutalisk", "Mutalisk", "Mutalisk", "Mutalisk", "Mutalisk", "Mutalisk", "Mutalisk", "Hatchery"]},
             "Zerg_3HatchScourge"    : { "Race" : "Zerg",    "OpeningBuildOrder" : ["Drone", "Drone", "Drone", "Drone", "Overlord", "Drone", "Drone", "Drone", "Drone", "Hatchery", "Drone", "Drone", "Spawning_Pool", "Drone", "Drone", "Extractor", "Drone", "Drone", "Drone", "Drone", "Drone", "Drone","Lair", "Drone", "Drone", "Drone", "Drone", "Drone", "Drone", "Drone", "Drone", "Drone", "Drone", "Spire", "Overlord", "Drone", "Overlord", "Hatchery", "Drone", "Drone", "Drone", "Drone", "Drone", "Drone", "Drone", "Drone", "Hatchery", "Drone", "Extractor", "Drone", "Hatchery", "Scourge", "Scourge", "Scourge", "Scourge", "Scourge", "Scourge", "Scourge", "Scourge", "Scourge", "Scourge", "Scourge", "Scourge", "Hatchery", "Extractor", "Extractor", "Hatchery"]}
         }
+
+
+Zerg_9Pool
+: 9/9 - Spawning Pool
+8/9 - Drone
+9/9 - Extractor Trick (see note) (optional)
+10/9 - Overlord
+10th Drone - Scout (optional) (see note)
+@100% Spawning Pool - 6 Zerglings and Natural Hatchery
+11/17 - two Zerglings (optional)
+        
+Zerg_3HatchHydra :
+9 - Overlord
+9 - Spawning Pool
+11 - Send out two drones. Hatchery (at natural) and scout with the other.
+10 - 2 Drones
+12 - 4 Zerglings
+15 - Hatchery at 3rd
+14 - Drone
+15 Extractor
+15 - Drone
+16 - Overlord
+@50 Gas - Hydralisk Den
+@100% Hydralisk Den - Hydralisk speed upgrade
+@70% Hydra speed upgrade make two Overlords
+@100% Hydralisk speed upgrade - Hydralisk range upgrade, move out
+
+Zerg_3HatchMuta:
+9 - Overlord
+12 - Hatchery @ expansion
+11-13 - Spawning Pool [1]
+13-14 - Hatchery
+12-14 - Extractor [2]
+@100% Spawning Pool - 2 Zerglings [3]
+16 - Overlord
+@100 Gas: - Lair
+21-23 - Extractor [4]
+@Next 100 Gas - Zergling Speed
+@100% Lair - Spire [5]
+24 - Overlord
+25..27 - Drones
+28..33 Zerglings
+33 - Expansion or Sunken Colonies to adapt to the Terran builds
+@50% Spire - 3 Overlords
+Save Larvae [6]
+@60% Spire - Send drone to expand to third [7]
+33ish - 9 Mutalisks
+@Next 200 Gas:
+2 more Mutalisks
+1 Overlord
+
+Protoss_DTDrop:
+8/9 — Pylon[1]
+10/17 - Gateway
+12/17 - Assimilator
+14/17 - Cybernetics Core
+15/17 - Pylon
+17/25 - Dragoon
+20/25 - Dragoon Range
+21/33 - Pylon
+22/33 - Dragoon
+25/33 - Dragoon
+29/33 - Nexus
+30/42 - Dragoon
+33/42 - Citadel of Adun
+33/42 - Robotics Facility
+@100% Citadel - Templar Archives and Gateway
+@100% Robotics Facility - Shuttle
+@100% Templar Archives - 2 Dark Templar
+
+Protoss_DTRush: 
+8 - Pylon
+10 - Gateway[1]
+12 - Assimilator
+13 - Zealot
+16 - Pylon
+18 - Cybernetics Core
+19 - Zealot
+22 - Pylon
+23 - Dragoon
+26 - Citadel
+27 - Dragoon[2]
+29 - Gateway
+29 - Pylon
+29 - Templar Archives
+2 Zealots[3][4]
+Pylon
+2 Dark Templar
+Forge
+Pylon[5]
+Nexus[6]
+Photon Cannons[7][6][8]
+
